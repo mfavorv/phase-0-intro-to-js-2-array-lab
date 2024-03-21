7ea03b426cd7a9e0cf3419a6df74ff8618b862dc
Favor Vincent <vfavorm77@gmail.com>
2024-03-22 01:35:03 +0300

    JS Array lab complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const cats2 = [...cats,"Broom"];
+    return cats2;
+}
+
+function prependCat(name){
+    const cats3 = ["Arnold", ...cats];
+    return cats3;
+}
+
+function removeLastCat(){
+    return cats.slice(0,2);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
